7054d5ac5ca9a53efe3acfcff60cbceb9ad52a2d
Joseph <josephwilli12@gmail.com>
2024-10-06 19:22:27 +0800

    setup jest tests

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env", // Transpile ES6+ to ES5
+    "@babel/preset-react", // Transpile JSX syntax
+    "@babel/preset-typescript", // Transpile TypeScript
+  ],
+};
